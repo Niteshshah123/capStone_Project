42e5e001bb88f4fd4ae3291d48f5c25cc635730a
Niteshshah123 <niteshshah011@gmail.com>
2024-04-07 19:36:15 +0545

    uploading capstrone project in github

---
+++ javastyle.js
@@ -0,0 +1,42 @@
+const username = document.querySelector("#username");
+const email = document.querySelector("#Email");
+const password = document.querySelector("#password");
+const loginbox = document.querySelector(".signinbox2");
+const loginbtn = document.querySelector("#Submit");
+var emailtype = /^[a-zA-Z0-9]+[@]+[a-zA-Z]+[\.]+[a-zA-Z]+$/;
+var usernametype = /^[A-Za-z\s]{2,}$/;
+function isvalid(){
+    if (usernametype.test(username.value) && emailtype.test(email.value) && username!="" && email!="" && password.value!="") {
+        alert("Sign Up Sucessfully!")
+        return true;
+    }
+    else if(username.value==""){
+        alert("User Name is Required");
+        return false;
+    }
+    else if(email.value==""){
+        alert("Email is Required");
+        return false;
+    }
+    else if(password.value==""){
+        alert("PassWord is Required");
+        return false;
+    }
+    else if(!usernametype.test(username.value)){
+        alert("Incorrect User Name");
+        return false;
+    }
+    else if(!emailtype.test(email.value)){
+        alert("Incorrect Email");
+        return false;
+    }
+    
+}
+loginbox.addEventListener("mouseenter", function(){
+    loginbox.style.backgroundColor="#B8B2EE";
+    loginbtn.style.backgroundColor="#B8B2EE";
+})
+loginbox.addEventListener("mouseleave", function(){
+    loginbox.style.backgroundColor="#fff";
+    loginbtn.style.backgroundColor="#fff";
+})
